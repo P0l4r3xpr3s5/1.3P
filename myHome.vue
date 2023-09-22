d02d7552616f1d3884712fa9b2e27d928b0b5301
P0l4r3xpr3s5 <141984452+P0l4r3xpr3s5@users.noreply.github.com>
2023-09-22 21:12:25 +1000

    ./views/myHome.vue

---
+++ myHome.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h2>Welcome to My Portfolio</h2>
+        <p>This is a brief introduction about myself.</p>
+
+        <h3>Feedback Form</h3>
+        <form @submit.prevent="submitFeedback">
+            <label for="email">Email:</label>
+            <br><input type="email" id="email" v-model="email">
+            <br>
+            <label for="feedback">Feedback:</label>
+            <br><textarea id="feedback" v-model="feedback"></textarea>
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "myHome",
+    data() {
+        return {
+            email: "",
+            feedback: ""
+        }
+    },
+    methods: {
+        submitFeedback() {
+            // Handle form submission logic here
+            console.log(this.email, this.feedback);
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+    color: #444;
+}
+</style>
